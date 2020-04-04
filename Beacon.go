ff6d728509995f40afff01e31f8ed87bdf1b3947
Andrew D <24252685+Donegaan@users.noreply.github.com>
2020-04-04 16:24:55 +0100

    Trying to run python files with full path

--- Beacon.go
+++ Beacon.go
@@ -85,13 +85,9 @@ func attendBufferChannel() {
             json.Unmarshal([]byte(j), &packet)
 
             log.Info(myIP.String() + " -> Message: " + packet.Message + " from " + packet.Source.String())
-            // cmd := exec.Command("python3 request_objects.py", "" + packet.Source.String())
-            cmd := exec.Command("pwd")
+            cmd := exec.Command("python3 /home/andrew/NS3DockerEmulator/docker/consumer/request_objects.py", packet.Source.String())
             err := cmd.Run()
             log.Info("Request object command finished with error: %v", err)
-            cmd1 := exec.Command("ls")
-            err1 := cmd1.Run()
-            log.Info("Request ls command finished with error: %v", err1)
         } else {
             fmt.Println("closing channel")
             done <- true
@@ -129,8 +125,8 @@ func beacon() {
         for {
             _,err = Conn.Write(buf)
             CheckError(err)
-            cmd := exec.Command("pwd")
-            // cmd := exec.Command("python3 publish_objects.py", "" + myIP.String() + " " + payload.Message)
+            streamObject := myIP.String() + " " + payload.Message
+            cmd := exec.Command("python3 /home/andrew/NS3DockerEmulator/docker/minimal/publish_objects.py", streamObject)
             err := cmd.Run()
             log.Info("Publish object command finished with error: %v", err)
             time.Sleep(time.Duration(r1.Intn(20)) * time.Second)
